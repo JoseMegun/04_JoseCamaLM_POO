9465efc944bb70ff4310b73347587493d860a6da
José Megun Cama La Madrid <111785479+JoseMegun@users.noreply.github.com>
2023-11-15 22:18:43 -0500

    Metodo Parametros y de Retorno - Jose Megun

--- 04_JoseCama_java/src/S07_04/persona1_04.java
+++ 04_JoseCama_java/src/S07_04/persona1_04.java
@@ -1,7 +1,9 @@
-
 package S07_04;
 
+// Jose Megun Cama La Madrid
+
 public class persona1_04 {
+    // Atributos
     private String nombre;
     private int edad;
 
@@ -11,6 +13,7 @@ public class persona1_04 {
         this.edad = edad;
     }
 
+    // Metodos
     public void imprimirInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
@@ -18,5 +21,20 @@ public class persona1_04 {
 
     public void saludar(String mensaje) {
         System.out.println("Hola, " + mensaje + ", soy " + nombre);
+    }
+
+    // Métodos con parametros
+    public void cumplirAños(int años) {
+        edad += años;
+        System.out.println("¡Feliz cumpleaños! Ahora tengo " + edad + " años.");
+    }
+
+    public void cambiarNombre(String nuevoNombre) {
+        nombre = nuevoNombre;
+        System.out.println("Me han cambiado el nombre a " + nombre);
+    }
+
+    public void estudiar(String materia) {
+        System.out.println(nombre + " está estudiando " + materia);
     }
 }
